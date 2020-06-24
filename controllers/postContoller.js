d637a8c037466b5030df214c1513380559619251
MarcoMercado <mmercad4@uncc.edu>
2020-06-24 09:56:29 -0400

    Connected delete and add function to mongodb

--- controllers/postContoller.js
+++ controllers/postContoller.js
@@ -1,45 +1,73 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{ title: 'test', content: 'lorem ipsumlorem lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum' },
+//connect to the database
+mongoose.connect('mongodb+srv://test:test@blog-hd4m3.mongodb.net/posts?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+//create a scheme - this is similar to a blueprint for the data
+
+var postSchema = new mongoose.Schema({
+    title: String, content: String
+});
+
+var Post = mongoose.model('Posts', postSchema);
+
+/* var data = [{ title: 'test', content: 'lorem ipsumlorem lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum' },
 { title: 'did homework', content: 'lorem ipsumlorem ipsumlolorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumrem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum ipsum' },
 { title: 'protest', content: 'lorem iplorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum' },
 { title: 'dinner at mom\'s', content: 'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum' }]
-
+ */
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
 
     app.get('/index', function (req, res) {
-        res.render('index', { posts: data })
+        //get data from mongodb and pass it to the view
+        Post.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('index', { posts: data })
+        });
     });
 
     //Posting content
     app.get('/add_post', function (req, res) {
-        res.render('add_post', {posts: data})
+        Post.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('add_post', { posts: data })
+        });
     });
 
     app.post('/add_post', urlencodedParser, function (req, res) {
-        data.push(req.body);
-        res.json(data);
+        var newPost = Post(req.body).save(function (err, data) {
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
     //Deleting content
-    app.get('/list_posts', function(req, res){
-        res.render('list_posts', {posts: data})
+    app.get('/list_posts', function (req, res) {
+        Post.find({}, function (err, data) {
+            if (err) throw err;
+            res.render('list_posts', { posts: data })
+        });
     });
 
     app.delete('/list_posts/:item', function (req, res) {
-        data = data.filter(function(postTitle){
-            newParam = req.params.item.replace("-", "");
-            return postTitle.title.replace(/ /g, '-') !== newParam;
-
+        //delete the requested item from Mongodb
+        newParam = req.params.item.replace("-", "");
+        Post.find({ title: newParam.replace(/\-/g, " ") }).remove(function (err, data) {
+            if (err) throw err;
+            res.json(data);
         });
-        res.json(data);
+        /*         data = data.filter(function(postTitle){
+                    newParam = req.params.item.replace("-", "");
+                    return postTitle.title.replace(/ /g, '-') !== newParam;
+         */
     });
 
-    app.get('/index', function (req, res) {
+app.get('/index', function (req, res) {
 
-    });
+});
 
 
 };
